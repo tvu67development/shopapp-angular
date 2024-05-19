3e5476e50382b95280a2b78ecdb1aa980aadd2b9
trungvu <trungquang.vu@dxc.com>
2024-05-19 17:05:18 +0700

    end video 64

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -42,11 +42,11 @@ import { TokenInterceptor } from './interceptors/token.interceptor';
     },
   ],
   bootstrap: [
-    HomeComponent,
-    //DetailProductComponent,
+    // HomeComponent,
+    DetailProductComponent,
     // OrderComponent,
     //OrderConfirmComponent,
-    LoginComponent,
+    // LoginComponent,
     // RegisterComponent
   ]
 })
---
+++ src/app/services/category.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core";
+import { environment } from "../environments/environment";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Category } from "../models/category";
+
+@Injectable({
+    providedIn: 'root'
+  })
+export class CategoryService {
+  private apiGetCategories  = `${environment.apiBaseUrl}/categories`;
+
+  constructor(private http: HttpClient) { 
+
+  }
+
+  getCategories(page: number, limit: number) : Observable<Category []> {
+    const params = new HttpParams() 
+    .set('page', page.toString())
+    .set('limit', limit.toString())
+    return this.http.get<Category[]>(this.apiGetCategories, {params});
+
+
+  }
+}
